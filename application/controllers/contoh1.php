dcf68b82c367983ebe51168ae83f83633ed5bc3b
selvianarsanti <dwiselviana770@gmail.com>
2023-10-15 11:06:09 +0700

    Upload Project pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Selviana Dwi Sari Arsanti 
+             Saya tinggal di Klaten 
+             Olahraga yang saya sukai adalah Bulutangkis"; 
+    }
+} 
